72bbdb6d3581c886818f38b94227815cd39bcd51
Fumiaki Kinoshita <fumiexcel@gmail.com>
2018-12-06 18:34:09 +0900

    WIP

--- src/Codec/EliasFano.hs
+++ src/Codec/EliasFano.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE LambdaCase #-}
 module Codec.EliasFano (EliasFano(..), unsafeFromVector, (!), prop_access) where
 
 import Control.Monad.ST
@@ -6,44 +8,48 @@ import Data.Bits
 import qualified Data.Vector.Generic as V
 import qualified Data.Vector.Unboxed as UV
 import qualified Data.Vector.Unboxed.Mutable as MV
+import qualified Data.Vector.Fusion.Bundle.Monadic as B
+import qualified Data.Vector.Fusion.Stream.Monadic as S
 import Data.Word
 
 import Codec.EliasFano.Internal
 
 import qualified Test.QuickCheck as QC
 
-unsafeFromVectorMax :: V.Vector v Int => Int -> v Int -> EliasFano
-unsafeFromVectorMax maxValue vec = runST $ do
+unsafeFromStreamMax :: Int -> S.Stream m Int -> m EliasFano
+unsafeFromStreamMax maxValue (S.Stream upd s0) = runST $ do
   let counterSize = 1 `unsafeShiftL` ceiling (logBase 2 $ fromIntegral len :: Double)
   mcounter <- MV.replicate counterSize 0
-  V.forM_ vec $ \v -> MV.unsafeModify mcounter (+1) (v `unsafeShiftR` width)
+
+  let go s = upd s >>= \case
+        S.Done -> pure S.Done
+        S.Skip s' -> pure $ S.Skip s'
+        S.Yield v s' -> do
+          MV.unsafeModify mcounter (+1) (v `unsafeShiftR` efWidth)
+          return $ S.Yield (B efWidth (fromIntegral v)) s'
   counter <- UV.unsafeFreeze mcounter
-  let upperVec = V.fromList $ build $ BitStream 0 (upper counter)
+  let efUpper = V.fromList $ build $ BitStream 0 (upper counter)
   return EliasFano
-    { efWidth = width
-    , efUpper = upperVec
-    , efRanks = UV.prescanl (+) 0 $ UV.map popCount upperVec
+    { efRanks = UV.prescanl (+) 0 $ UV.map popCount upperVec
     , efLower = V.fromList $ build $ BitStream 0 lower
+    , ..
     }
   where
     len = V.length vec
-    width = max 1 $ ceiling $ logBase 2 (fromIntegral maxValue / fromIntegral len :: Double)
-    lower i
-      | i == len = Done
-      | otherwise = Yield width (fromIntegral $ V.unsafeIndex vec i) (i + 1)
+    efWidth = max 1 $ ceiling $ logBase 2 (fromIntegral maxValue / fromIntegral len :: Double)
     upper counter i
-      | i == V.length counter = Done
-      | otherwise = let n = V.unsafeIndex counter i in Yield (n + 1) (mask n) (i + 1)
-{-# INLINE unsafeFromVectorMax #-}
-
-unsafeFromVector :: V.Vector v Int => v Int -> EliasFano
-unsafeFromVector vec
+      | i == V.length counter = S.Done
+      | otherwise = let n = V.unsafeIndex counter i in S.Yield (n + 1) (mask n) (i + 1)
+{-
+unsafeFromStream :: V.Vector v Int => v Int -> EliasFano
+unsafeFromStream vec
   | V.null vec = unsafeFromVectorMax 1 vec
   | otherwise = unsafeFromVectorMax (V.last vec + 1) vec
 {-# SPECIALISE unsafeFromVector :: UV.Vector Int -> EliasFano #-}
-
+-}
 data EliasFano = EliasFano
-    { efWidth :: !Int
+    { efLength :: !Int
+    , efWidth :: !Int
     , efUpper :: !(UV.Vector Word64)
     , efRanks :: !(UV.Vector Int)
     , efLower :: !(UV.Vector Word64)
@@ -51,7 +57,7 @@ data EliasFano = EliasFano
     deriving Show
 
 (!) :: EliasFano -> Int -> Int
-EliasFano width upper ranks lower ! i = unsafeShiftL (select ranks upper i - i) width
+EliasFano _ upper ranks lower ! i = unsafeShiftL (select ranks upper i - i) width
   .|. fromIntegral (readBits lower width (i * width))
 
 prop_access :: [QC.NonNegative Int] -> QC.NonNegative Int -> QC.Property
